a71016efbc0252c66ae7beff600e5f94af3d8013
austinjnr <austinobonyo@gmail.com>
2023-02-11 02:09:38 +0300

    Assignment completed

--- control_flow.rb
+++ control_flow.rb
@@ -37,5 +37,17 @@ end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
