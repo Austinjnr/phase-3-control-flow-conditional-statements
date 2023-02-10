68e4ae06cb864e62fe13ee92b872c45ab28c471e
austinjnr <austinobonyo@gmail.com>
2023-02-11 01:39:07 +0300

    passed first condition with if and else

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,19 @@
+require 'pry'
 def admin_login(username, password)
   # your code here
+  if username == 'admin' || username == 'ADMIN' && password == '12345'
+    "Access denied"
+  else 
+    "Access denied"
+  end
 end
+
+binding.pry
 
 def hows_the_weather(temperature)
   # your code here
+  case temperature
+
 end
 
 def fizzbuzz(num)
