e296d7ab2c94767d19125dcc27d7ecd61c266039
austinjnr <austinobonyo@gmail.com>
2023-02-11 02:01:50 +0300

    passed second  condition with if and else

--- control_flow.rb
+++ control_flow.rb
@@ -1,20 +1,30 @@
-require 'pry'
+
+
 def admin_login(username, password)
   # your code here
-  if username == 'admin' || username == 'ADMIN' && password == '12345'
-    "Access denied"
+  if username == 'admin' && password == '12345'
+    "Access granted"
+  elsif username == 'ADMIN' && password == '12345'
+    "Access granted"
   else 
     "Access denied"
   end
 end
 
-binding.pry
-
 def hows_the_weather(temperature)
   # your code here
-  case temperature
-
+    if temperature < 40
+      "It's brisk out there!"
+    elsif temperature >= 40 && temperature <= 65
+      "It's a little chilly out there!"
+    elsif temperature > 85
+      "It's too dang hot out there!"
+    else 
+      "It's perfect out there!"
+    end
 end
+
+
 
 def fizzbuzz(num)
   # your code here
